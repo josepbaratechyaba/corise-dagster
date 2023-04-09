ad87a80ebf942f292dd496070e7a26b1291e7623
josep baratech <josep.baratech@goyaba.co>
2023-04-09 19:05:15 +0000

    project1

--- week_1/project/week_1.py
+++ week_1/project/week_1.py
@@ -50,26 +50,32 @@ def csv_helper(file_name: str) -> Iterator[Stock]:
             yield Stock.from_list(row)
 
 
+@op(description="take stock list from S3", config_schema={"s3_key": str})
+def get_s3_data_op(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    stocks = list(csv_helper(s3_key))
+    return stocks
+
+
+@op(description="from a list of stocks as an input return an aggregation with the highest value")
+def process_data_op(context, stocks: List[Stock]) -> Aggregation:
+    max_value = max(stocks, key=lambda stock: stock.high)
+    ag = Aggregation(date=max_value.date, high=max_value.high)
+    return ag
+
+
 @op
-def get_s3_data_op():
+def put_redis_data_op(context, stock: Aggregation):
     pass
 
 
 @op
-def process_data_op():
-    pass
-
-
-@op
-def put_redis_data_op():
-    pass
-
-
-@op
-def put_s3_data_op():
+def put_s3_data_op(context, stock: Aggregation):
     pass
 
 
 @job
 def machine_learning_job():
-    pass
+    res = process_data_op(get_s3_data_op())
+    put_redis_data_op(res)
+    put_s3_data_op(res)
